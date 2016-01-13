fe669589148bc7e240935655e0fa1405254ff3d8
jahidul <jsec516@gmail.com>
2016-01-13 14:59:51 +0600

    updated

---
+++ src/vendor.ts
@@ -0,0 +1,14 @@
+require('./lib/bootstrap/less/bootstrap.less');
+require('./css/main.css');
+
+import 'angular2/bundles/angular2-polyfills';
+
+import 'angular2/platform/browser';
+import 'angular2/core';
+import 'angular2/http';
+import 'angular2/router';
+import 'gsap';
+
+// FOR WHATEVER REASON, THIS MUST COME AFTER THE ANGULAR2-POLYFILLS IMPORT
+require('./lib/bootstrap/jquery-1.11.3.min.js')
+require('imports?jQuery=jquery!./lib/bootstrap/bootstrap.min.js');
